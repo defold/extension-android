ae7142c8c97ed497748ad43f618c5355f6206e17
Sven Andersson <sven.andersson@king.com>
2017-03-24 14:31:18 +0100

    Added source for Dummy.jar

---
+++ java_src/com/svenandersson/dummy/Dummy.java
@@ -0,0 +1,10 @@
+package com.svenandersson.dummy;
+
+/**
+ * Created by svenandersson on 24/02/17.
+ */
+public class Dummy {
+    static public String DoStuff() {
+        return "Hello from the other siiiiiiide!!!";
+    }
+}
